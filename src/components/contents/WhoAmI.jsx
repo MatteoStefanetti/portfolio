66574cd9884a2c1747d5420a87d5444b63371ac6
Matteo Stefanetti <matteo.stefanetti@edu.unito.it>
2025-03-01 23:16:48 +0100

    Position of backcard text

--- src/components/contents/WhoAmI.jsx
+++ src/components/contents/WhoAmI.jsx
@@ -3,7 +3,7 @@ import {Container, Image} from "react-bootstrap";
 
 const WhoAmI = () => {
     return (
-        <Container fluid className="d-flex align-content-around">
+        <Container fluid className="d-flex justify-content-center">
             <div className="flip-card">
                 <div className="flip-card-inner">
                     <div className="flip-card-front">
@@ -13,9 +13,9 @@ const WhoAmI = () => {
                     </div>
                     <div className="flip-card-back">
                         <div className="card rounded-circle h-100 w-100">
-                            <div className="card-body">
+                            <div className="card-body d-flex flex-column justify-content-center">
                                 <p className="card-title">Hi, I&#39;m Matteo Stefanetti</p>
-                                <p className="card-text">I&#39;m a backend programmer</p>
+                                <p className="card-text">I&#39;m a backend programmer and I&#39;ve studied at University of Turin</p>
                             </div>
                         </div>
                     </div>
