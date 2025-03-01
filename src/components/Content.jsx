0ee7aedae0e922693f7cade64128e1960b63f475
Matteo Stefanetti <matteo.stefanetti@edu.unito.it>
2025-03-01 18:05:17 +0100

    Update project

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,14 +1,33 @@
 import 'react';
-import {Container, Image} from "react-bootstrap";
+import {Card, Col, Container, Image, Row, Stack} from "react-bootstrap";
 import WhoAmI from "./contents/WhoAmI.jsx";
+
+const logos = ['https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original-wordmark.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/spring/spring-original-wordmark.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/arduino/arduino-original-wordmark.svg'];
 
 const Content = () => {
     return (
-            <Container fluid={true} className="content-bg flex-grow-1">
-                <WhoAmI />
-                <Container>
-
-                </Container>
+            <Container fluid={true} className="content-bg flex-grow-1 py-5">
+                <Stack gap={4}>
+                    <WhoAmI />
+                    <Container>
+                        <Row>
+                            {Array.from({ length: logos.length }).map((_, i) => (
+                                <Col key={i}>
+                                    <Container fluid>
+                                        <Image src={logos[i]} />
+                                    </Container>
+                                </Col>
+                            ))}
+                        </Row>
+                    </Container>
+                </Stack>
             </Container>
     );
 };
--- src/components/contents/WhoAmI.jsx
+++ src/components/contents/WhoAmI.jsx
@@ -3,8 +3,25 @@ import {Container, Image} from "react-bootstrap";
 
 const WhoAmI = () => {
     return (
-        <Container fluid>
-            <Image src="public/images/Profile_matteo3.jpeg" roundedCircle alt="My Image" className="border border-5" height="300"></Image>
+        <Container>
+            <div className="flip-card">
+                <div className="flip-card-inner">
+                    <div className="flip-card-front">
+                        <div className="card rounded-circle">
+                            <Image src="images/Profile_matteo3.jpeg" className="card-img rounded-circle" />
+                        </div>
+                    </div>
+                    <div className="flip-card-back">
+                        <div className="card rounded-circle">
+                            <div className="card-body">
+                                <p className="card-title">Hi, I&#39;m Matteo Stefanetti</p>
+                                <p className="card-text">I&#39;m a backend programmer</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </Container>
     )
 }
