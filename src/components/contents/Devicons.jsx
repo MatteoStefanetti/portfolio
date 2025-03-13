4b1253311116ce3ec6481a868428b58e464208e0
Matteo Stefanetti <matteo.stefanetti@edu.unito.it>
2025-03-13 11:26:10 +0100

    Modified responsiveness of devicons

--- src/components/contents/Devicons.jsx
+++ src/components/contents/Devicons.jsx
@@ -8,16 +8,16 @@ const logos = ['https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgr
     'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg',
     'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg',
     'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg',
-    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/arduino/arduino-original-wordmark.svg'];
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/axios/axios-plain.svg'];
 
 const Devicons = () => {
     return(
         <Container>
-            <Row className="d-flex justify-content-between">
+            <Row className="d-flex justify-content-between my-3">
                 {Array.from({ length: logos.length }).map((_, i) => (
-                    <Col sm={3} lg={1} key={i} className="flex-fill">
-                        <Container>
-                            <Image className="m-3 mw-100" width="100px" src={logos[i]} />
+                    <Col xs={6} sm={3} xl={1} key={i} className="d-flex justify-content-center my-2">
+                        <Container fluid className="border border-1 rounded-4 p-1 border-success d-flex justify-content-center align-items-center" style={{width: "100px"}}>
+                            <Image className="rounded-4" width="100px" src={logos[i]} />
                         </Container>
                     </Col>
                 ))}
